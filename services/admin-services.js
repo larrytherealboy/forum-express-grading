51f40a4706aec887f35ed057bc5615d43af10e49
larrytherealboy <abclarryabclarry@gmail.com>
2023-06-01 12:38:43 +0800

    feat: add create restaurant api

--- services/admin-services.js
+++ services/admin-services.js
@@ -1,4 +1,5 @@
 const { Restaurant, Category } = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const adminController = {
   getRestaurants: (req, cb) => {
@@ -8,6 +9,23 @@ const adminController = {
       include: [Category]
     })
       .then(restaurants => cb(null, { restaurants }))
+      .catch(err => cb(err))
+  },
+  postRestaurant: (req, cb) => {
+    const { name, tel, address, openingHours, description, categoryId } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+    const { file } = req // 把檔案取出來，也可以寫成 const file = req.file
+    imgurFileHandler(file) // 把取出的檔案傳給 file-helper 處理後
+      .then(filePath => Restaurant.create({ // 產生一個新的 Restaurant 物件實例，並存入資料庫
+        name,
+        tel,
+        address,
+        openingHours,
+        description,
+        image: filePath || null,
+        categoryId
+      }))
+      .then(newRestaurant => cb(null, { restaurant: newRestaurant })) // 新增完成後導回後台首頁
       .catch(err => cb(err))
   },
   deleteRestaurant: (req, cb) => {
